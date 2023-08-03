741b9f4a2fb48cbbd7cb487284fff28323e38343
Tellulu <Tellulu1564959850@gmail.com>
2023-08-03 19:31:37 +0800

    完成了装饰模式

--- LearnCpp/MyDecorator/MyDecorator.cpp
+++ LearnCpp/MyDecorator/MyDecorator.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
+	ConcreteComponentMetal* metal = new ConcreteComponentMetal();
+	ConcreteComponentPunk* punk = new ConcreteComponentPunk();
+	ConcreteComponentRock* rock = new ConcreteComponentRock();
+	ConcreteDecoratorAvant_grade ag(metal);
+	ConcreteDecoratorPost postpunk(punk);
+	ConcreteDecoratorPost postrock(rock);
 
+
+	std::cout << ag.GetStyle() << std::endl;
+	std::cout << postpunk.GetStyle() << std::endl;
+	std::cout << postrock.GetStyle() << std::endl;
 
 	return 0;
 }
--- LearnCpp/MyDecorator/MyDecorator.h
+++ LearnCpp/MyDecorator/MyDecorator.h
@@ -2,6 +2,79 @@
 #ifndef _MYDECORATOR_
 #define _MYDECORATOR_
 
+#include<iostream>
 
+class Component
+{
+public:
+	virtual ~Component(){}
+
+	virtual std::string GetStyle() const = 0;
+};
+
+class ConcreteComponentMetal : public Component
+{
+public:
+	virtual std::string GetStyle() const override
+	{
+		return "Metal";
+	}
+};
+
+class ConcreteComponentPunk : public Component
+{
+public:
+	virtual std::string GetStyle() const override
+	{
+		return "Punk";
+	}
+};
+
+class ConcreteComponentRock : public Component
+{
+public:
+	virtual std::string GetStyle() const override
+	{
+		return "Rock";
+	}
+};
+
+class Decorator : public Component
+{
+public:
+	Decorator(Component* component):component_(component){}
+
+	virtual std::string GetStyle() const override
+	{
+		return this->component_->GetStyle();
+	}
+
+
+private:
+	Component* component_;
+};
+
+class ConcreteDecoratorPost : public Decorator
+{
+public:
+	ConcreteDecoratorPost(Component* component):Decorator(component){}
+
+	virtual std::string GetStyle() const override
+	{
+		return "Post " + Decorator::GetStyle();
+	}
+
+};
+
+class ConcreteDecoratorAvant_grade : public Decorator
+{
+public:
+	ConcreteDecoratorAvant_grade(Component* component) :Decorator(component) {}
+
+	virtual std::string GetStyle() const override
+	{
+		return "Avant_grade " + Decorator::GetStyle();
+	}
+};
 
 #endif // !_MYDECORATOR_
